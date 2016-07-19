dd6ef94c547a5e4de6b1f5852c6abf9cf56a3df8
sukh dol <sukh.dol77@gmail.com>
2016-07-19 05:30:36 -0600

    Add data-annotations to the customer name.

--- Vidly/Vidly/Models/Customer.cs
+++ Vidly/Vidly/Models/Customer.cs
@@ -1,8 +1,13 @@
-﻿namespace Vidly.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Your name is required.")]
+        [StringLength(255, ErrorMessage = "Your name cannot exceed 255 characters")]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public byte MembershipTypeId { get; set; }
