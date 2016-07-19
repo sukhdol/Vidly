c69b5fea5e1962dbe437f06f7623480e7f3e1885
sukh dol <sukh.dol77@gmail.com>
2016-07-19 17:19:09 -0600

    Membership type name is now required.

--- Vidly/Vidly/Models/MembershipType.cs
+++ Vidly/Vidly/Models/MembershipType.cs
@@ -1,11 +1,18 @@
-﻿namespace Vidly.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
 {
     public class MembershipType
     {
         public byte Id { get; set; }
+
+        [Required(ErrorMessage = "You need to provide the name for the Membership Type.")]
         public string Name { get; set; }
+
         public short SignUpFee { get; set; }
+
         public byte DurationInMonths { get; set; }
+
         public byte DiscountRate { get; set; }
     }
 }
