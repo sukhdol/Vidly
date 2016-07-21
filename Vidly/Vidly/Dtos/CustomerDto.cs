1041abdc5c5412bd4e971766c977d4e4745f0a99
sukh dol <sukh.dol77@gmail.com>
2016-07-21 16:32:29 -0600

    Add CustomerDto and mapped customer using AutoMapper.

---
+++ Vidly/Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter customer's name.")]
+        [StringLength(255, ErrorMessage = "The customer name cannot exceed 255 characters.")]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        [Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+
+        [Required(ErrorMessage = "The membership type is required.")]
+        public byte MembershipTypeId { get; set; }
+    }
+}
