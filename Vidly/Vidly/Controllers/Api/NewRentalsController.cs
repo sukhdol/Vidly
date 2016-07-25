73e5aa9560780ca0beb440a4e1dc1e97f3760e32
sukh dol <sukh.dol77@gmail.com>
2016-07-25 01:34:46 -0600

    Add simple create rentals action.

--- Vidly/Vidly/Controllers/Api/NewRentalsController.cs
+++ Vidly/Vidly/Controllers/Api/NewRentalsController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Linq;
+using System.Web.Http;
 using Vidly.Dtos;
 using Vidly.Models;
 
@@ -16,6 +18,23 @@ namespace Vidly.Controllers.Api
         [HttpPost]
         public IHttpActionResult CreateNewRentals(NewRentalsDto newRental)
         {
+            var customer = _context.Customers.Single(c => c.Id == newRental.CustomerId);
+
+            var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id));
+
+            foreach (var movie in movies)
+            {
+                var rental = new Rental
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now
+                };
+
+                _context.Rentals.Add(rental);
+            }
+
+            _context.SaveChanges();
 
             return Ok();
         }
