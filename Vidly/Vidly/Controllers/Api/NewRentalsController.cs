01d9d0977713ba609bc4c1d8a4233d266d64fb12
sukh dol <sukh.dol77@gmail.com>
2016-07-25 01:51:26 -0600

    Number of Available rentals cannot be less than 1.

--- Vidly/Vidly/Controllers/Api/NewRentalsController.cs
+++ Vidly/Vidly/Controllers/Api/NewRentalsController.cs
@@ -20,10 +20,15 @@ namespace Vidly.Controllers.Api
         {
             var customer = _context.Customers.Single(c => c.Id == newRental.CustomerId);
 
-            var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id));
+            var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id)).ToList();
 
             foreach (var movie in movies)
             {
+                if (movie.NumberAvailable == 0)
+                {
+                    return BadRequest("Movie is not available.");
+                }
+
                 movie.NumberAvailable--;
 
                 var rental = new Rental
